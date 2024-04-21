680f9a4c62ff76b29a9d9c9a980bf87bac46f13a
fisiognomico <gipri@riseup.net>
2024-04-21 18:50:29 +0200

    Command parsing and error definition

---
+++ src/cli.rs
@@ -0,0 +1,56 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+use crate::errors::Errcode;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "crabcan", about = "A simple container in Rust.")]
+pub struct Args {
+    /// Activate debug mode
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// Command to execute inside the container
+    #[structopt(short, long)]
+    pub command: String,
+
+    /// User ID to create inside the container
+    #[structopt(short, long)]
+    pub uid: u32,
+
+    #[structopt(parse(from_os_str), short = "a", long = "add")]
+    pub addpaths: Vec<PathBuf>,
+
+    /// Directory to mount as root of the container
+    #[structopt(parse(from_os_str), short = "m", long = "mount")]
+    pub mount_dir: PathBuf,
+}
+
+pub fn parse_args() -> Result<Args, Errcode> {
+    let args = Args::from_args();
+
+    // If args.debug: Setup log at debug level
+    // Else: Setup log at info level
+    if args.debug{
+        setup_log(log::LevelFilter::Debug);
+    } else {
+        setup_log(log::LevelFilter::Info);
+    }
+
+    // Validate arguments
+    if !args.mount_dir.exists() || !args.mount_dir.is_dir(){
+        return Err(Errcode::ArgumentInvalid("mount"));
+    }
+
+    if args.command.is_empty() {
+        return Err(Errcode::ArgumentInvalid("command"));
+    }
+
+    Ok(args)
+}
+
+pub fn setup_log(level: log::LevelFilter){
+    env_logger::Builder::from_default_env()
+        .format_timestamp_secs()
+        .filter(None, level)
+        .init();
+}
